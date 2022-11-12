07c2e804d6a792e16f92fae01e44887e796fc904
Gustavo Salinas <gsalinas.phys@gmail.com>
2022-11-12 16:04:38 +0100

    Initial sampling comparing metropolis with scipy

---
+++ sampling.py
@@ -0,0 +1,23 @@
+import random
+from typing import Callable, Generator
+
+import numpy as np
+
+
+# Proposal function for Metropolis–Hastings algorithm, using here a gaussian with width sigma
+def proposal(x: np.ndarray, sigma: float = 0.1) -> np.ndarray:
+    return np.random.normal(x, sigma)
+
+# Metropolis–Hastings algorithm for sampling from a probability distribution
+def metropolis(pdistr: Callable, n: int, x0: np.ndarray, sigma: float = 1., burn_in: int = 10_000) -> Generator:
+    x = x0 # start somewhere
+    for _ in range(n+burn_in):
+        trial = proposal(x, sigma) # random neighbor from the proposal distribution
+        acceptance = pdistr(trial) / pdistr(x)
+        
+        # accept the move conditionally
+        if random.random() < acceptance:
+            x = trial
+
+        if n > burn_in:
+            yield x
